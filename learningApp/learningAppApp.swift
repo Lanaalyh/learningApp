6e88cf814b397fc8f1a6e66e50f0976acafd87e3
Lanaalyh <lanaa20055@gmail.com>
2024-10-28 20:41:39 +0300

    Initial Commit

---
+++ learningApp/learningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningAppApp.swift
+//  learningApp
+//
+//  Created by Lana Alyahya on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct learningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learningAppTests/learningAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  learningAppTests.swift
+//  learningAppTests
+//
+//  Created by Lana Alyahya on 28/10/2024.
+//
+
+import Testing
+@testable import learningApp
+
+struct learningAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
